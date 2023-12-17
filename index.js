37a08874ae7fd4889b45f41ef74975c2f2b460b7
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-17 12:59:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 "use strict"
-function includes(array, value) {
-  // Change code below this line
-  let number = 0;
-for (const item of array) {
-  if (item===value) {
-   number = item
-  }
-}
-return number === value
-}
+
+
+function add(...args) {
 
   // Change code above this line
+}
 
 
-
-  // Change code above this line
-const result = includes([1, 2, 3, 4, 5], 3)
+const result = add(15, 27)
 console.log(result);
+
 
