c2243eef1028c91940d6f110fae5715e073067c6
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-19 19:12:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 "use strict"
 
 
-function add(...args) {
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  addPotion(potionName) {
+    // Change code below this line
+ this.potions.push(potionName);
+ return this.potions
+    // Change code above this line
+  },
+  removePotion(potionName) {
+let index = this.potions.indexOf(potionName);
 
-  // Change code above this line
+if (index!== -1) {
+  this.potions.splice(index, 1)
 }
+return this.potions;
+  },
+  updatePotionName(oldName, newName) {
+    let index = this.potions.indexOf(oldName);
 
+if (index!== -1) {
+  this.potions.splice(index, 1, newName)
+}
+return this.potions;
+  }
+  
+};
 
-const result = add(15, 27)
+const result = atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
 console.log(result);
 
 
