d8da3b34c6f8ae7c3eaebcc0bbc14a8f1227f6f1
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-22 18:25:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,89 @@
 "use strict"
 
 
-function changeEven(numbers, value) {
-  // Change code below this line
-  let newArr = []
-  numbers.forEach((item)=> {
-    if (item % 2 === 0) {
-      item = item + value
-    }
-    newArr.push(item)
-  })
-  return newArr;
-  // Change code above this line
-}
 
+const getUsersWithAge = (users, minAge, maxAge) => {
+  const arr = users.filter(user => {
+   return user.age >= minAge || user.age <= maxAge  
+  }
+   
+  )
+console.log(arr);
+};
 
+const result = getUsersWithAge([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+], 20, 30
+)
+  console.log(result);
 
-const result = changeEven([1, 2, 3, 4, 5], 10)
-console.log(result);
-
-
+ 
