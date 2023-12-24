a58da9a7076564fc971b4855331a20ec32a7dbc8
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-24 11:29:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,88 +2,77 @@
 
 
 
-const getUsersWithAge = (users, minAge, maxAge) => {
-  const arr = users.filter(user => {
-   return user.age >= minAge || user.age <= maxAge  
-  }
-   
-  )
-console.log(arr);
-};
+function sortByProperty(arr){
+return arr.filter((item) => item.interests.length)
 
-const result = getUsersWithAge([
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male",
-    age: 37
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female",
-    age: 34
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male",
-    age: 24
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female",
-    age: 21
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male",
-    age: 27
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male",
-    age: 38
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female",
-    age: 39
-  }
-], 20, 30
-)
-  console.log(result);
+// .sort((a, b) => b - a)
+// .map((item) => item.name)
+}
 
- 
+
+const result = sortByProperty([
+  {
+    name:'Olga',
+    age: '31',
+    student: true,
+    interests: ['JS', 'reading', 'family', 'friends']
+  },
+  {
+    name:'Alex',
+    age: '36',
+    student: false,
+    interests: ['moto', 'fising', 'family']
+  },
+  {
+    name:'Ivan',
+    age: '6',
+    student: false,
+    interests: ['cartoons', 'toys']
+  },
+])
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books.filter((book)=> book.rating > MIN_BOOK_RATING).map((book)=>book.author).sort((a,b)=> a.localeCompare(b));
+
+// console.log(names);
