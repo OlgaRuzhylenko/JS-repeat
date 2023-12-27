fea1d55c4b807ed95c976694a80cad64bbdfbeb6
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-27 19:43:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 "use strict"
 
-
-function booleanToString(b){
-return b.toString()
-
-}
-const result = booleanToString(true)
+function squareDigits(num){
+    const digits = [];
+    while (num > 0) {
+        const digit = num % 10;
+        digits.unshift(digit); // Додаємо цифру в початок масиву
+        // num = Math.floor(num / 10);
+      }
+    return digits
+  }
+const result = squareDigits(919)
 console.log(result);
 
 
