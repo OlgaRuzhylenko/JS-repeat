f8ad59bca3bd4a0c705e5f68811029421bd9b6fe
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-12-27 15:23:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,107 +1,13 @@
 "use strict"
-// Знаходження простих чисел:
-// Напишіть функцію, яка приймає число і повертає true, якщо воно є простим, і false в іншому випадку.
-
-// Обернення слів у рядку:
-// Напишіть функцію, яка обертає кожне слово в рядку, залишаючи порядок слів незмінним.
-
-// Обчислення суми чисел Фібоначчі:
-// Напишіть функцію, яка приймає число n і повертає суму перших n чисел Фібоначчі.
-
-// Визначення найбільшого спільного дільника (НСД):
-// Напишіть функцію, яка приймає два числа і повертає їхній найбільший спільний дільник (НСД).
-
-// Сортування рядка за кількістю входжень кожного символу:
-// Напишіть функцію, яка приймає рядок і повертає новий рядок, в якому символи впорядковані за зростанням їхньої кількості входжень у вихідному рядку.
 
 
-function foo(number){
-let summ = 0;
-for (let i = 0; i < number; i++) {
-  summ = i + 
+function booleanToString(b){
+return b.toString()
+
 }
-}
-
-const result = foo(5)
+const result = booleanToString(true)
 console.log(result);
 
 
 
 
-
-
-
-
-
-
-// const result = sortByProperty([
-//   {
-//     name:'Olga',
-//     age: 31,
-//     student: true,
-//     interests: ['JS', 'reading', 'family', 'friends']
-//   },
-//   {
-//     name:'Alex',
-//     age: 36,
-//     student: false,
-//     interests: ['moto', 'fising', 'family']
-//   },
-//   {
-//     name:'Ivan',
-//     age: 6,
-//     student: false,
-//     interests: ['cartoons', 'toys']
-//   },
-//   {
-//     name:'vovva',
-//     student: false,
-//     interests: ['cartoons']
-//   },
-// ])
-// console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     rating: 8.38,
-//   },
-//   {
-//     title: "Beside Still Waters",
-//     author: "Robert Sheckley",
-//     rating: 8.51,
-//   },
-//   {
-//     title: "The Dream of a Ridiculous Man",
-//     author: "Fyodor Dostoevsky",
-//     rating: 7.75,
-//   },
-//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-//   {
-//     title: "The Dreams in the Witch House",
-//     author: "Howard Lovecraft",
-//     rating: 8.67,
-//   },
-// ];
-// const MIN_BOOK_RATING = 8;
-// // Change code below this line
-
-// const names = books.filter((book)=> book.rating > MIN_BOOK_RATING).map((book)=>book.author).sort((a,b)=> a.localeCompare(b));
-
-// console.log(names);
