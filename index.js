878477ce26c4ae09df4aa8bdc9f036d626eecac7
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-01-29 08:26:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 "use strict"
-function removeExclamationMarks(s) {
-return s.split('!').join('');
+function XO(str) {
+   const arr = str.toLowerCase().split('');
+   let Xarr = [];
+   let OArr = [];
+for (let i = 0; i < arr.length; i+=1) {
+    if (arr[i] === 'x') {
+        Xarr.push(arr[i]);
+    } else {
+        OArr.push(arr[i]);
+    }
+}
+if (Xarr.length === OArr.length) {
+    return true
+} else {
+    return false
+}
 }
 
-const result = removeExclamationMarks("He!llo! World!")
+const result = XO("zzoo");
 console.log(result);
