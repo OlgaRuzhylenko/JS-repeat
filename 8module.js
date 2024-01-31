6168e0fedd14f8c5f0d55df0983fbffa64237b12
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-01-31 20:05:14 +0200

    1

---
+++ 8module.js
@@ -0,0 +1,37 @@
+const cars = [
+      {
+        id: 1,
+        car: "Honda",
+        type: "Civic",
+        price: 12000,
+    img: 'https://wallpaper.mob.org.ua/pc/image/vehicles-honda_civic_type_r-car-honda-white_car-420818.html'
+      },
+      {
+        id: 2,
+        car: "Audi",
+        type: "Q7",
+        price: 40000,
+    img: 'https://wallpaper.mob.org.ua/pc/image/vehicles-audi_q7-311915.html'
+      },
+      {
+        id: 3,
+        car: "BMW",
+        type: "5 series",
+        price: 9000,
+    img: 'https://wallpaper.mob.org.ua/pc/image/cars-bmw_5_series-bmw_5-bmw-belii-vid_sboku-oblaka-115889.html'
+      },
+      {
+        id: 4,
+        car: "Honda",
+        type: "Accord",
+        price: 20000,
+    img: 'https://wallpaper.mob.org.ua/pc/image/vehicles-honda_accord-car-compact_car-honda-white_car-425341.html'
+      },
+      {
+        id: 5,
+        car: "Volvo",
+        type: "XC60",
+        price: 7000,
+    img: 'https://wallpaper.mob.org.ua/pc/image/vehicles-volvo_xc60-car-suv-volvo-white_car-425193.html'
+      },
+      ]
