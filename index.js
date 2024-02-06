e08db079ee94d6304f38588f2d4c346ca4858a04
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-02-06 09:01:43 +0200

    4

--- index.js
+++ index.js
@@ -7,19 +7,18 @@
 // const result = uniqueInOrder("AAAABBBCCDAABBB");
 // console.log(result);
 // =================================================
-// import { tasks } from "./tasks";
   const tasks = [
     { id: 1, task: "Закінчити проект", completed: false },
     { id: 2, task: "Приготувати обід", completed: true },
     { id: 3, task: "Вивчити JavaScript", completed: false },
     { id: 4, task: "Погуляти на свіжому повітрі", completed: true },
   ];
-const list = document.querySelector('.js-list');
+const list = document.querySelector('.js-list'); 
 
 function taskMarcup(arr) {
     const markup = arr.map(({id, task, completed }) => 
-        ` <li class="js-item">
-      <p clas="js-task">${task}</p>
+        ` <li class="js-item" data-id="${id}">
+      <p class="js-task">${task}</p>
       <li class="js-btn">
         <button class="js-done">Done</button>
         <button class="js-close">Close</button>
@@ -28,4 +27,22 @@ function taskMarcup(arr) {
         
    list.innerHTML = markup
 }
-taskMarcup(tasks)
+taskMarcup(tasks);
+
+list.addEventListener('click', onListClick);
+
+function onListClick(evt) {
+  // console.log(evt.target);
+  if (evt.target.classList.contains('js-done')) {
+    const { id } = evt.target.closest('.js-item').dataset;
+    const taskElement = evt.target.closest('.js-item').querySelector('.js-task');
+    console.log(taskElement);
+    const task = findTask(Number(id));
+    
+  taskElement.classList.add('completed');
+  }
+}
+
+function findTask(taskId) {
+return tasks.find(({ id })=> id === taskId)
+}
