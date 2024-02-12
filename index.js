e1237ad39bdf2ec64185b28e9580aa91b55c4ab8
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-02-12 20:28:50 +0200

    51

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 "use strict";
 
-const tasks = [];
+const tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
 const listElem = document.querySelector(".js-list");
 const formElem = document.querySelector(".js-form-container");
+const statusSelect = document.getElementById("status");
 
 formElem.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  // console.log(evt.target.elements);
   const item = {
     id: evt.target.elements.id.value,
     task: evt.target.elements.task.value,
-    status: evt.target.elements.status.value,
   };
-  console.log(item);
 
   const markup = templateTask(item);
   listElem.insertAdjacentHTML("beforeend", markup);
 
-  tasks.push(item)
+  tasks.push(item);
+
+  evt.target.elements.id.value = "";
+  evt.target.elements.task.value = "";
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function templateTask(item) {
   return `
  <li class="js-item " data-id="${item.id}">
-      <p class="js-task completed">${item.task}</p>
+      <p class="js-task">${item.task}</p>
       <li class="js-btn">
         <button class="js-done">Done</button>
         <button class="js-close">Close</button>
@@ -38,44 +40,38 @@ function markupArr(arr) {
   return arr.map(templateTask).join("");
 }
 
-// const list = document.querySelector('.js-list');
-// const keyTasksStyle = 'task status'
+function renderArr(arr) {
+  const markup = markupArr(arr);
 
-// function taskMarcup(arr) {
-//     const markup = arr.map(({id, task, completed }) =>
-//         ` <li class="js-item " data-id="${id}">
-//       <p class="js-task ${completed ? 'completed' : ''}">${task}</p>
-//       <li class="js-btn">
-//         <button class="js-done">Done</button>
-//         <button class="js-close">Close</button>
-//       </li>
-//     </li>`).join('');
+  listElem.innerHTML = markup;
+}
 
-//    list.innerHTML = markup
-// }
-// taskMarcup(tasks);
+renderArr(tasks);
 
-// list.addEventListener('click', onListClick);
+listElem.addEventListener("click", onListClick);
 
-// function onListClick(evt) {
-//   if (evt.target.classList.contains('js-done')) {
-//     const { id } = evt.target.closest('.js-item').dataset;
-//     const taskElement = evt.target.closest('.js-item').querySelector('.js-task');
-//     const task = findTask(Number(id));
-//     task.completed = !task.completed;
-//     const completed = taskElement.classList.toggle('completed');
+function onListClick(evt) {
+  if (evt.target.classList.contains("js-done")) {
+    const { id } = evt.target.closest(".js-item").dataset;
+    const taskElement = evt.target.closest(".js-item").querySelector(".js-task");
+    const task = findTask(Number(id));
+    const completed = taskElement.classList.toggle("completed");
+    task.completed = completed;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-//     localStorage.setItem(keyTasksStyle, JSON.stringify(tasks))
-//   }
+  if (evt.target.classList.contains("js-close")) {
+    const { id } = evt.target.closest(".js-item").dataset;
+    const taskItem = document.querySelector(".js-item");
+    tasks.splice(
+      tasks.findIndex((task) => task.id === parseInt(id)),
+      1
+    );
+    taskItem.remove();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+}
 
-//   if (evt.target.classList.contains('js-close')) {
-//     const { id } = evt.target.closest('.js-item').dataset;
-//     const taskItem = document.querySelector('.js-item');
-
-//     taskItem.remove();
-//   }
-// }
-
-// function findTask(taskId) {
-// return tasks.find(({ id })=> id === taskId)
-// }
+function findTask(taskId) {
+  return tasks.find(({ id }) => id === taskId);
+}
